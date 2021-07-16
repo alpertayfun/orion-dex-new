b42469b264769b0b195e9e276fa4bad25999b962
Alper Tayfun <tayfun.alper@gmail.com>
2021-07-16 13:11:42 +0300

    a

--- src/views/Home/components/TotalValueLockedCard.tsx
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -20,8 +20,8 @@ const Row = styled.div`
 const TotalValueLockedCard = () => {
   const { t } = useTranslation()
   const data = useGetStats()
-  const totalLiquidity = 18550 
-  const dailyVolume = 3200
+  const totalLiquidity = 18800 
+  const dailyVolume = 3450
 
   return (
     <StyledTotalValueLockedCard>
